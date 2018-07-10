3756f73ef52abbce65f3589c6d0e24fa30d0b44e
Unknown <avivaslois@deloitte.es>
2018-07-10 11:38:43 +0200

    Code upload

---
+++ Catalog.Domain/ControllerWorkers/PhoneCW.cs
@@ -0,0 +1,76 @@
+﻿using Catalog.Domain.DTO;
+using Catalog.Infrastructure.DTO;
+using Catalog.Infrastructure.DTO.Mappers;
+using Catalog.Infrastructure.Factories;
+using Catalog.Infrastructure.Models;
+using Catalog.Infrastructure.Models.Catalog.Infrastructure.Models;
+using Catalog.Infrastructure.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Catalog.Domain.ControllerWorkers
+{
+    public class PhoneCW: IPhoneCW
+    {
+        private UnitOfWork<PhoneContext> Uow = new UnitOfWork<PhoneContext>(new PhoneContextFactory());
+        private readonly IRepository<Phone> _phoneRepository;
+
+        public PhoneCW()
+        {
+            _phoneRepository = Uow.GetRepository<Phone>();
+        }
+
+        public PhoneCW(IRepository<Phone> mockRepository)
+        {
+            _phoneRepository = mockRepository;
+        }
+
+        public List<PhoneDto> getAllPhones()
+        {
+            try
+            {
+                List<PhoneDto> result;
+
+                result = _phoneRepository.FindAll().Select(x => DTOMapper.Phone_To_PhoneDto(x)).ToList();
+
+                return result;
+            }
+            catch (Exception ex)
+            { 
+                throw ex;
+            }
+        }
+
+        public PhoneDto getPhoneById(int id)
+        {
+            try
+            {
+                PhoneDto result = null;
+                var data = _phoneRepository.FindById(id);
+                if (data != null) { result = DTOMapper.Phone_To_PhoneDto(data); }
+                return result;
+            }
+            catch (Exception ex)
+            {           
+                throw ex;
+            }
+        }
+
+        public List<PhonePriceDto> getPricesByIds(List<int> ids)
+        {
+            try
+            {
+                List<PhonePriceDto> data = _phoneRepository.FindAll(x => ids.Any(y => y == x.Id))
+                                                                 .Select(z => new PhonePriceDto { PhoneId= z.Id, PhonePrice= z.Price }).ToList();
+                return data;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Order.Domain/ControllerWorkers/Interfaces/IOrderCW.cs
@@ -0,0 +1,16 @@
+﻿using Order.Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Order.Domain.ControllerWorkers
+{
+    public interface IOrderCW
+    {
+        Task<bool> processOrder(OrderDto order);
+        Task<List<OrderDetailResultDto>> getPhonePrices(List<OrderDetailDto> details);
+        List<OrderDetailResultDto> completeResultInfo(OrderDto order, List<OrderDetailResultDto> detailsResults);
+        string compositeMessage(CustomerDto customer, List<OrderDetailResultDto> detailsResults);
+    }
+}
